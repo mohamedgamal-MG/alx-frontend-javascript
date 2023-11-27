e645ae382a86514885e005721f2ab3c860a479d4
mohamedgamal-MG <mohamedgamal.jemmy.zero@gmail.com>
2023-11-27 23:13:09 +0000

    8-getBudgetCurrentYear

--- 0x00-ES6_basic/8-getBudgetCurrentYear.js
+++ 0x00-ES6_basic/8-getBudgetCurrentYear.js
@@ -1,14 +1,14 @@
 function getCurrentYear() {
-    const date = new Date;
-    return date.getFullYear();
+  const date = new Date();
+  return date.getFullYear();
 }
 
 export default function getBudgetForCurrentYear(income, gdp, capita) {
-    const buget = {
-        [`income-${getCurrentYear()}`]: income,
-        [`gdp-${getCurrentYear()}`]: gdp,
-        [`capita-${getCurrentYear()}`]: capita
-    };
+  const budget = {
+    [`income-${getCurrentYear()}`]: income,
+    [`gdp-${getCurrentYear()}`]: gdp,
+    [`capita-${getCurrentYear()}`]: capita,
+  };
 
-    return buget;
+  return budget;
 }
