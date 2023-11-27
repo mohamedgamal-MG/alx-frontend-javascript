101bdfc8765d3604c39617a6183e4a7546d7c83b
mohamedgamal-MG <mohamedgamal.jemmy.zero@gmail.com>
2023-11-27 17:40:47 +0000

    01-blockScope

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,11 +1,9 @@
-export default function taskBlock (trueOrFalse) {
-	let task = false;
-	let task2 = true;
+export default function taskBlock(trueOrFalse) {
+    const task = false;
+    const task2 = true;
 
-	if (trueOrFalse) {
-		return [task, task2];
-	}
-	
-	return [task, task2];
+    if (trueOrFalse) {
+    return [task, task2];
+    }
+    return [task, task2];
 }
-
