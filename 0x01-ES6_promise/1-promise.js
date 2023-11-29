b64c81328cce7d0a810e7383f75e480c5373262b
mohamedgamal-MG <mohamedgamal.jemmy.zero@gmail.com>
2023-11-29 13:01:31 +0000

    1-promise

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,9 +1,6 @@
 export default function getResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({ status: 200, body: 'Success' });
-    } else {
+    resolve({ status: 200, body: 'Success' }),
       reject(new Error('The fake API is not working currently'));
-    }
   });
 }
