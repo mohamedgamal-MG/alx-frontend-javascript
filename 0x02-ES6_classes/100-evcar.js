a09301a5ae6aca4a8e95b6f9c93cbbd90c4f4b26
mohamedgamal-MG <mohamedgamal.jemmy.zero@gmail.com>
2023-11-30 21:30:38 +0000

    100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -3,20 +3,14 @@ import Car from './10-car';
 export default class EVCar extends Car {
   constructor(brand, motor, color, range) {
     super(brand, motor, color);
-    this.range = range;
+    this._range = range;
   }
 
-  get range() {
-    return this._range;
-  }
-
-  set range(value) {
-    this._range = value;
+  static get [Symbol.species]() {
+    return Car;
   }
 
   cloneCar() {
-    const Species = super.constructor[Symbol.species];
-
-    return new Species();
+    return new this.constructor[Symbol.species]();
   }
 }
